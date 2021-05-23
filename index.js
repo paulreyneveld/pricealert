826edd95a1f3539f75eaf2d5d59991e1d8e9bf90
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-23 15:39:25 -0700

    Ibid

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 const axios = require('axios');
-// import { IEXClient } from 'iex-api'
-const iexAPI = require('iex-api');
-const _fetch = require('isomorphic-fetch');
-console.log(iexAPI);
-
-const iex = new iexAPI.IEXClient(_fetch)
 
 const getData = async () => {
     return await axios.get('https://baconipsum.com/api/?type=meat-and-filler')
 }
 
-const getStockInfo = () => {
-    iex.stockCompany('AAPL')
-        .then(company => console.log(company))
-        .catch(error => console.log(error))
-}
-
 const run = async () => {
     // const data = await getData()
     // console.log(data.data)
-    getStockInfo()
     
 }
 
