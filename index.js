7dd0835253f8f7cf50978f27ae82ef8cd2482b5f
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-23 15:41:35 -0700

    Added support for hidden environment variables

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const axios = require('axios');
 
 const getData = async () => {
     return await axios.get('https://baconipsum.com/api/?type=meat-and-filler')
 }
-
+console.log(process.env.TEST)
 const run = async () => {
     // const data = await getData()
     // console.log(data.data)
