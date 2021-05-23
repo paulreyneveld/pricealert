6be4f74cd07b3b5eb44962665ab6b4e90affd23a
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-23 15:49:35 -0700

    Proof of concept for data via alpha advantage

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 require('dotenv').config()
 const axios = require('axios');
 
-const getData = async () => {
+const getTestData = async () => {
     return await axios.get('https://baconipsum.com/api/?type=meat-and-filler')
+}
+
+const getStockData = async () => {
+    return await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${process.env.API_KEY}`)
 }
 console.log(process.env.TEST)
 const run = async () => {
-    // const data = await getData()
-    // console.log(data.data)
-    
+    const testData = await getTestData()
+    console.log(testData.data)
+    const stockData = await getStockData()
+    console.log(stockData.data)
 }
 
 run()
